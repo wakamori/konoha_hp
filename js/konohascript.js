c68bc920869febab354200aacacc88ef15ba90fa
utrhira <utr.hira@gmail.com>
2012-02-24 04:55:38 +0900

    fix columnbox

--- js/konohascript.js
+++ js/konohascript.js
@@ -27,12 +27,50 @@ function konohascript_init()
 				.css("clear", "both"));
 
 	$(".konoha-menu span")
+		.css({ width: (100/($(".konoha-menu span").size())+"%"),
+			   height: ($(".konoha-menu").css("height")-1) })
 		.wrapAll(
 			$("<div>")
 				.attr("class", "konoha-menu-wrapper"));
 
 	$(".konoha-footer")
-		.html("Copyright © 2012 Konoha Project");
+		.append(
+			$("<div>")
+				.attr("class", "uh-columnbox")
+				.each(function(){
+					var container = $(this);
+					var items = $(".konoha-menu div span");
+					jQuery.each(items, function() {
+						var text = $(this).text();
+						container
+							.append(
+								$("<div>")
+									.css("text-align", "center")
+									.append(
+										$("<ul>")
+											.attr("class", "konoha-footer-list")
+											.append(
+												$("<li>")
+													.attr("class", "konoha-footer-lhead")
+													.text(text))
+											.each(function() {
+												/* ----- dummy generation ----- */
+												var list = $(this);
+												var count = Math.floor(Math.random() * 4) + 1;
+												for (var i = 0; i < count; i++) {
+													list
+														.append(
+															$("<li>")
+																.attr("class", "konoha-footer-litem")
+																.text("dummy"));
+												}
+											})));
+					});
+				}))
+		.append(
+			$("<div>")
+				.attr("class", "konoha-footer-copyright")
+				.html("Copyright © 2012 Konoha Project"))
 };
 
 function yoan_modules_init() {
@@ -52,7 +90,7 @@ function uh_modules_init() {
 	$(".uh-columnbox")
 		.each(function() {
 			var children = $(this).children();
-			var par = (100/(children.size())-1.0)+"%";
+			var par = (100/(children.size()))+"%";
 			children
 				.css({ width: par,
 					   float: "left" })
@@ -68,7 +106,7 @@ function uh_modules_init() {
 		.each(function() {
 			var p = $(this);
 			p.css("text-align", "left");
-			var keywords = ["Konoha", "JavaScript", "Java", "Ruby", "Python", "Perl", "C++", "C"];
+			var keywords = ["Konoha", "JavaScript", "Java", "Ruby", "Python", "Perl", "C++"];
 			jQuery.each(keywords, function() {
 				p.highlight(this);
 			});
@@ -84,6 +122,7 @@ function uh_modules_init() {
 							   fontFamily:   "monaco",
 							   fontSize:     "13px",
 							   padding:      "10px",
+							   margin:       "15px",
 							   border:       "2px double rgba(255,255,255,1.0)",
 							   boxShadow:    "1px 1px 3px rgba(0,0,0,0.8)",
 							   borderRadius: "8px" })));
