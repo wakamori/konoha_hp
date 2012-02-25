c24f35b573560ff645f36f59327baf7628c5c9c6
utrhira <utr.hira@gmail.com>
2012-02-25 19:31:32 +0900

    refix content displaying time

--- js/main.js
+++ js/main.js
@@ -2,17 +2,19 @@
 ; /* <- DON'T ERASE THIS */
 
 $(document).ready(function() {
+	/* ----- display contents elements ----- */
+	$(document.body)
+		.css("display", "block");
+
+	/* ----- load all modules ----- */
 	konohascript_init();
 	uh_modules_init();
 	yoan_modules_init();
 	goccy_modules_init();
 
-	/* --- all modules initialized --- */
-	/* display contents elements */
-	$(document.body)
-		.css("display", "block");
+	/* ----- all modules loaded ----- */
 	setTimeout(function() {
 		$(".konoha-contents")
-			.animate({ opacity: 1.0 }, 400);
+			.animate({ opacity: 1.0 }, 300);
 	}, 400);
 });
