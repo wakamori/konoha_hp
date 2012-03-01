6cd63a1273a9e8d16890f10686bb416218f1efeb
utrhira <utr.hira@gmail.com>
2012-03-01 20:17:03 +0900

    update download & uh.js: add uh-csvtable

--- htdocs/js/uh.js
+++ htdocs/js/uh.js
@@ -14,6 +14,31 @@ function uh_modules_init() {
 						.css("clear", "both"));
 		});
 
+	$(".uh-csvtable")
+		.each(function() {
+			var target = $(this);
+			var csv = $.csv()($.trim(target.text()));
+			target.text("");
+			var table = $("<table>");
+			function replaceLink(text) {
+				if (text.indexOf("http://") == 0) {
+					text = $("<a>")
+						.attr("href", text)
+						.text("[link]")
+						.get(0).outerHTML;
+				}
+				return text;
+			};
+			$(csv).each(function(idx) {
+				var tr = $("<tr>");
+				$.each(this, function() {
+					tr.append($((idx == 0) ? "<th>" : "<td>").html(replaceLink($.trim(this))));
+				});
+				table.append(tr);
+			});
+			target.append(table);
+		});
+
 	$(".uh-script")
 		.each(function() {
 			var script = $(this).text();
