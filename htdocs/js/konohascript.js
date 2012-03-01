17af41201400b24a86ea4d23253d5277130dd1cd
Masaaki Goshima <goccy54@gmail.com>
2012-03-01 15:44:59 +0900

    update dir's layout

---
+++ htdocs/js/konohascript.js
@@ -0,0 +1,66 @@
+; /* <- DON'T ERASE THIS */
+
+function konohascript_init()
+{
+	$(".konoha-header")
+		.append(
+			$("<img>")
+				.attr("src", "images/logo.png")
+				.attr("class", "konoha-header-logo"))
+		.append(
+			$("<div>")
+				.append(
+					$("<span>")
+						.text("Konoha")
+						.attr("class", "konoha-header-title"))
+				.append(
+					$("<span>")
+						.text("A Statically-typed Scripting Langauge")
+						.attr("class", "konoha-header-subtitle")))
+		.each(function() {
+			$(this).children()
+				.css("float", "left");
+		})
+		.append(
+			$("<div>")
+				.css("clear", "both"));
+
+	$(".konoha-menu span")
+		.attr("class", "konoha-menu-item")
+		.css("width", (100/($(".konoha-menu-item").size())+"%"))
+		.wrapAll(
+			$("<div>")
+				.attr("class", "konoha-menu-wrapper"));
+
+	$(".konoha-contents p")
+		.each(function() {
+			var p = $(this);
+			var keywords = ["Konoha", "JavaScript", "Java", "Ruby", "Python", "Perl", "C++"];
+			$.each(keywords, function() {
+				p.highlight(this);
+			});
+		});
+
+	$(".konoha-footer")
+/*		.append($("<div>").css("padding-bottom","50px")
+				.append($("<div>").css({float:"left",width:"30%"})
+						.append($("<img>").attr("src","images/logo.png").css("width","50px"))
+						.append($("<span>").text("Konoha").css({fontSize:"28px",fontWeight:"bold",marginLeft:"10px"})))
+				.append($("<div>").css({float:"left",width:"70%"})
+						.append($("<div>").attr("class","uh-columnbox")
+								.each(function(){
+									var container = $(this);
+									$(".konoha-menu-item").each(function() {
+										var text = $(this).text();
+										container.append($("<div>")
+														 .append($("<ul>").css({textAlign:"left",listStyleType:"none"})
+																 .each(function(){
+																	 $(this)
+																		 .append($("<li>").css({fontWeight:"bold",marginBottom:"3px"}).text(text))
+																		 .append($("<li>").css("margin","1px 12px").text("dummy"));})));});})))
+				.append($("<div>").css("clear","both")))*/
+		.append(
+			$("<div>")
+				.attr("class", "konoha-footer-copyright")
+				.html("Copyright © 2012 Konoha Project"));
+};
