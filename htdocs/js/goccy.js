d9985e9f4545e5cf417f2b713b4eecc332046b36
Masaaki Goshima <goccy54@gmail.com>
2012-03-02 23:30:13 +0900

    update demo

--- htdocs/js/goccy.js
+++ htdocs/js/goccy.js
@@ -27,7 +27,7 @@ function getActualDimension(image) {
 function goccy_modules_init() {
 	$(".konoha-event-timeline-sub")
 		.each(function() {
-			var text = $(this).children(".konoha-event-timeline-text")
+			var text = $(this).children(".konoha-event-timeline-text");
 			var title_h = $(this).children(".konoha-event-timeline-subtitle").height();
 			var h = text.height();
 			var image = $(this).children("img");
@@ -51,4 +51,68 @@ function goccy_modules_init() {
 			$(this).css({ height: title_h*2+h+"px"});
 		});
 	$(".konoha-event-timeline-main").append("<div>&nbsp;</div>");
-};
+	$(".konoha-demo-contents-subtitle")
+		.each(function() {
+			var rand_color = "#";
+			for (var i = 0; i < 3; i++) {
+				var rand = Math.floor(Math.random() * 155);
+				rand += 100;
+				rand_color += rand.toString(16);
+			}
+			$(this).css({ borderBottom: "3px solid" + rand_color});
+		});
+	$(".konoha-demo-contents img").click(function(event){
+		//popup lightbox video
+		var video_path = $(this).attr("id");
+		var popup_window = document.createElement("div");
+
+		var video = document.createElement("video");
+		video.className = "konoha-demo-contents-video";
+		video.src = "../video/" + video_path + ".mov";
+		video.controls = true;
+
+		var codearea = document.createElement("span");
+		codearea.className = "konoha-demo-contents-codearea";
+		
+		popup_window.appendChild(video);
+		popup_window.appendChild(codearea);
+
+		var x = window.innerWidth / 2;
+		var y = window.innerHeight / 2;
+		popup_window.style.width = "1px";
+		popup_window.style.height = "10px";
+		popup_window.style.top = y + "px";
+		popup_window.style.left = x + "px";
+		var after_x = x - 448;
+		var after_y = y - 256;
+		popup_window.className = "konoha-lightbox-window";
+		document.getElementById("body").appendChild(popup_window);
+		var filter = document.createElement("div");
+		filter.className = "konoha-lightbox-filter";
+		filter.style.width = window.innerWidth + "px";
+		filter.style.height = window.innerHeight + "px";
+		document.getElementById("body").appendChild(filter);
+		$(".konoha-lightbox-filter").click(function(event) {
+			$(this).remove();
+			$(".konoha-lightbox-window").remove();
+		});
+		$(".konoha-lightbox-filter").animate({
+			opacity:1.0,
+		}, 1000);
+		$(".konoha-lightbox-window").animate({
+			top:after_y + "px",
+			width: "1px",
+			height:"512px",
+		}, 500, function() {
+			$(this).animate({
+				left:after_x + "px",
+				width: "896px",
+			}, 500, function() {
+				$(".konoha-demo-contents-codearea").append("<u>preview this sample's code</u>");
+				$(".konoha-demo-contents-codearea u").click(function(event) {
+					
+				});
+			});
+		});
+	});
+}
